9cd724bdce569cf768bf27a7fbb1b14a909bb380
Tamer Kilinc <kilinc@sipgate.de>
2023-12-11 20:11:34 +0100

    :sparkles: Added function for rendering consultant tags

--- src/hmpu-plugin.js
+++ src/hmpu-plugin.js
@@ -1,4 +1,59 @@
 document.addEventListener("DOMContentLoaded", function () {
+  hmpuIframeSettings();
+  renderConsultantTags();
+});
+
+function renderConsultantTags() {
+  const consultantTagElements = document.querySelectorAll(
+    ".hmpu-consultant-tag"
+  );
+
+  if (!consultantTagElements) {
+    console.warn(
+      "hmpu-Plugin Error: Could not find any consultant tags. Please make sure the tags are set and have the correct class."
+    );
+    return;
+  }
+
+  consultantTagElements.forEach((tagElement) => {
+    const consultantId = tagElement.getAttribute("data-consultant-id");
+
+    if (!consultantId) {
+      console.warn(
+        "hmpu-Plugin Error: Could not find consultant ID. Please make sure the ID is set and has the correct attribute."
+      );
+      return;
+    }
+
+    const xhr = new XMLHttpRequest();
+    const url = `https://happy-mpu.web.app/api/consultant/${consultantId}/tags`;
+    xhr.open("GET", url, true);
+    xhr.addEventListener("load", () => {
+      if (xhr.status !== 200) {
+        console.warn(
+          "hmpu-Plugin Error: Could not load consultant tags. Please make sure the ID is correct."
+        );
+        return;
+      }
+
+      const { tags } = JSON.parse(xhr.responseText);
+
+      let tagsHTML = "";
+      tags.forEach((tag) => {
+        tagsHTML += `<span class="hmpu-consultant-tag">${tag}</span>`;
+      });
+
+      tagElement.innerHTML = `
+        <div class="hmpu-consultant-tag-container">
+        ${tagsHTML}
+        </div>
+        `;
+    });
+    xhr.send();
+  });
+}
+
+function hmpuIframeSettings() {
   const iframe = document.getElementById("hmpu-plugin-iframe");
 
   if (!iframe) {
@@ -25,4 +80,4 @@ document.addEventListener("DOMContentLoaded", function () {
       iframe.style.minHeight = `${e.data.data.height + 50}px`;
     }
   });
-});
+}
